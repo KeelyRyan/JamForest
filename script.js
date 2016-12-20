93b609889fcce07442d35bf855e6605d532b793c
KeelyRyan <keelyannryan@gmail.com>
2016-12-20 18:15:08 +0000

    JQuery nav started

---
+++ script.js
@@ -0,0 +1,22 @@
+
+$('.class1a').mouseover(function() {
+  $('.text1').css("visibility","visible");
+});
+
+$('.class1a').mouseout(function() {
+  $('.text1').css("visibility","hidden");
+});
+$('.class1b').mouseover(function() {
+  $('.text2').css("visibility","visible");
+});
+
+$('.class1b').mouseout(function() {
+  $('.text2').css("visibility","hidden");
+});
+$('.class1c').mouseover(function() {
+  $('.text3').css("visibility","visible");
+});
+
+$('.class1c').mouseout(function() {
+  $('.text3').css("visibility","hidden");
+});
